dce6a7782fc4a3549bfcc136a0080ae836975ed9
jimmykodes <jkeith@kochava.com>
2022-01-27 14:43:11 -0800

    feat: look for default recipe file

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # mock-sponse
-an api response mocking server
+An api response mocking server
+
+## Example Recipe File
+
+```yaml
+routes:
+  - path: /
+    sequential: true
+    responses:
+      - code: 200
+        data: |
+          {
+            "some": "data",
+            "to": "return"
+          }
+      - code: 500
+        data: "{\"error\": \"something went wrong\"}"
+  - path: /animals/{name:[a-zA-Z]}/
+    responses:
+      - code: 200
+        data: |
+          {
+            "animal": "bear",
+            "name": "{{ index .Vars "name" }}"
+          }
+  - path: /vehicles/
+    responses:
+      - code: 401
+        file: ../permission_denied.json
+```
--- main.go
+++ main.go
@@ -4,25 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jimmykodes/mock-sponse/internal/server"
 )
 
 func main() {
-	port := flag.Int("port", 5000, "port to run server on")
+	port := flag.Int("p", 5000, "port to run server on")
+	file := flag.String("f", "", "specify a recipe file other than recipe.(yaml|yml)")
 	flag.Parse()
 
-	args := flag.Args()
-	var filename string
-
-	switch len(args) {
-	case 1:
-		filename = args[0]
-	default:
-		fmt.Println("invalid number of args")
+	filename, err := getFile(*file)
+	if err != nil {
+		log.Println(err)
 		return
 	}
-
 	svr, err := server.New(filename, *port)
 	if err != nil {
 		log.Println("error initializing server", err)
@@ -32,4 +28,23 @@ func main() {
 		log.Println("error running server", err)
 		return
 	}
+}
+
+func getFile(fromFlag string) (string, error) {
+	if fromFlag != "" {
+		_, err := os.Stat(fromFlag)
+		if err != nil {
+			return "", fmt.Errorf("could not find specified recipe file")
+		} else {
+			return fromFlag, nil
+		}
+	} else {
+		if _, err := os.Stat("recipe.yaml"); err == nil {
+			return "recipe.yaml", nil
+		}
+		if _, err := os.Stat("recipe.yml"); err == nil {
+			return "recipe.yml", nil
+		}
+	}
+	return "", fmt.Errorf("no valid recipe file found")
 }
