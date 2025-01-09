3d608e7d0118db6cdb36e3f2ab7d3b629af05197
jimmykodes <jimmy@khanacademy.org>
2025-01-09 14:08:10 -0800

    feat: support route params

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,6 +9,7 @@ type Route struct {
 	Value     string
 	Handlers  map[string]http.Handler
 	subroutes map[string]*Route
+	wild      *Route
 }
 
 type Router struct {
@@ -27,15 +28,19 @@ func newRoute(value string) *Route {
 func (ro *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
 
-	route := ro.Top
+	last := ro.Top
 	for _, part := range parts {
-		route = route.subroutes[part]
+		route := last.subroutes[part]
+		if route == nil {
+			route = last.wild
+		}
 		if route == nil {
 			ro.NotFoundHandler.ServeHTTP(w, r)
 			return
 		}
+		last = route
 	}
-	if handler := route.Handlers[r.Method]; handler != nil {
+	if handler := last.Handlers[r.Method]; handler != nil {
 		handler.ServeHTTP(w, r)
 	} else {
 		ro.NotFoundHandler.ServeHTTP(w, r)
@@ -48,8 +53,23 @@ func (ro *Router) Register(pattern string, method string, handler http.Handler) 
 	for _, part := range parts {
 		route, ok := last.subroutes[part]
 		if !ok {
-			route = newRoute(part)
-			last.subroutes[part] = route
+			if len(part) > 0 && part[0] == ':' {
+				// is wildcard
+				if last.wild == nil {
+					route = newRoute(part)
+					last.wild = route
+				} else {
+					if last.wild.Value == part {
+						route = last.wild
+					} else {
+						// TODO: bubble up the error correctly
+						panic("multiple wild routes")
+					}
+				}
+			} else {
+				route = newRoute(part)
+				last.subroutes[part] = route
+			}
 		}
 		last = route
 	}
